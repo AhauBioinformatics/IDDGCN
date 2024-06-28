0d7615ac46d3f01449b56543e0977ae5b3f73698
GQ4376 <1278740784@qq.com>
2024-06-28 19:32:29 +0800

    update

--- explanation/ground_truth.py
+++ explanation/ground_truth.py
@@ -4,11 +4,10 @@ from concurrent.futures import ProcessPoolExecutor
 import numpy as np
 
 def construct_ground_truth(dc_row, dd, cc, dc):
-    # For cells in one of the edge dc_row DC, find cells similar to this cell in CC and return to the list
-    sim_cellines1 = cc.loc[(cc['cell1'] == dc_row['cell']), 'cell2'].to_list()
-    sim_cellines2 = cc.loc[(cc['cell2'] == dc_row['cell']), 'cell1'].to_list()
-    similar_cell_lines = sim_cellines1 + sim_cellines2
-    print('len(similar_cell_lines)',len(similar_cell_lines))
+    sim_mutation1 = cc.loc[(cc['mu1'] == dc_row['mu']), 'mu2'].to_list()
+    sim_mutation2 = cc.loc[(cc['mu2'] == dc_row['mu']), 'mu1'].to_list()
+    similar_mutation = sim_mutation1 + sim_mutation2
+    print('len(similar_mutation)',len(similar_mutation))
 
     sim_drugs1 = dd.loc[(dd['drug1'] == dc_row['drug']), 'drug2'].to_list()
     sim_drugs2 = dd.loc[(dd['drug2'] == dc_row['drug']), 'drug1'].to_list()
@@ -21,58 +20,57 @@ def construct_ground_truth(dc_row, dd, cc, dc):
     # Create a DataFrame to hold g round truth data
     ground_truth_data = []
 
-    # Check for dc entries that match the original drug and similar cell lines
-    for cell_line in similar_cell_lines:
-        matched_entries = dc.loc[(dc['drug'] == dc_row['drug']) & (dc['cell'] == cell_line) & (dc['rel'] == dc_row['rel'])]
-        for _, matched_row in matched_entries.iterrows():
-            ground_truth_data.append((matched_row['cell'],matched_row['rel'],dc_row['drug']))
-            added_combinations.add((matched_row['drug'], dc_row['cell']))
 
-    # Check for dc entries that match similar drugs and the original cell line
-    for drug in similar_drugs:
-        matched_entries = dc.loc[(dc['drug'] == drug) & (dc['cell'] == dc_row['cell']) & (dc['rel'] == dc_row['rel'])]
+    for mutation in similar_mutation:
+        matched_entries = dc.loc[(dc['drug'] == dc_row['drug']) & (dc['mu'] == mutation) & (dc['rel'] == dc_row['rel'])]
         for _, matched_row in matched_entries.iterrows():
-            ground_truth_data.append((dc_row['cell'],matched_row['rel'],matched_row['drug']))
-            added_combinations.add((matched_row['drug'], dc_row['cell']))
+            ground_truth_data.append((matched_row['mu'],matched_row['rel'],dc_row['drug']))
+            added_combinations.add((matched_row['drug'], dc_row['mu']))
 
-    # Check for dc entries that match similar drugs and similar cell lines
+
     for drug in similar_drugs:
-        for cell_line in similar_cell_lines:
-            matched_entries = dc.loc[(dc['drug'] == drug) & (dc['cell'] == cell_line) & (dc['rel'] == dc_row['rel'])]
+        matched_entries = dc.loc[(dc['drug'] == drug) & (dc['mu'] == dc_row['mu']) & (dc['rel'] == dc_row['rel'])]
+        for _, matched_row in matched_entries.iterrows():
+            ground_truth_data.append((dc_row['mu'],matched_row['rel'],matched_row['drug']))
+            added_combinations.add((matched_row['drug'], dc_row['mu']))
+
+
+    for drug in similar_drugs:
+        for mutation in similar_mutation:
+            matched_entries = dc.loc[(dc['drug'] == drug) & (dc['mu'] == mutation) & (dc['rel'] == dc_row['rel'])]
             for _, matched_row in matched_entries.iterrows():
-                ground_truth_data.append((matched_row['cell'],matched_row['rel'],matched_row['drug']))
-                added_combinations.add((matched_row['drug'], matched_row['cell']))
+                ground_truth_data.append((matched_row['mu'],matched_row['rel'],matched_row['drug']))
+                added_combinations.add((matched_row['drug'], matched_row['mu']))
 
-    # Check for similar drug relationships with original and similar cell lines
+
     for drug in similar_drugs:
-        # Check against original cell line
-        if any(dc.loc[(dc['drug'] == drug) & (dc['cell'] == dc_row['cell']), 'rel'] == dc_row['rel']):
+        if any(dc.loc[(dc['drug'] == drug) & (dc['mu'] == dc_row['mu']), 'rel'] == dc_row['rel']):
             combination = (dc_row['drug'], drug)
             if combination not in added_combinations:
                 ground_truth_data.append((dc_row['drug'], 2,drug))  # 2 indicates similarity
                 added_combinations.add(combination)
-        # Check against similar cell lines
-        for cell_line in similar_cell_lines:
-            if any(dc.loc[(dc['drug'] == drug) & (dc['cell'] == cell_line), 'rel'] == dc_row['rel']):
+
+        for mutation in similar_mutation:
+            if any(dc.loc[(dc['drug'] == drug) & (dc['mu'] == mutation), 'rel'] == dc_row['rel']):
                 combination = (dc_row['drug'], drug)
                 if combination not in added_combinations:
                     ground_truth_data.append((dc_row['drug'], 2, drug))  # 2 indicates similarity
                     added_combinations.add(combination)
 
-    # Check for similar cell line relationships with original and similar drugs
-    for cell_line in similar_cell_lines:
+    # Check for similar mutation relationships with original and similar drugs
+    for mutation in similar_mutation:
         # Check against original drug
-        if any(dc.loc[(dc['drug'] == dc_row['drug']) & (dc['cell'] == cell_line), 'rel'] == dc_row['rel']):
-            combination = (dc_row['cell'], cell_line)
+        if any(dc.loc[(dc['drug'] == dc_row['drug']) & (dc['mu'] == mutation), 'rel'] == dc_row['rel']):
+            combination = (dc_row['mu'], mutation)
             if combination not in added_combinations:
-                ground_truth_data.append((dc_row['cell'], 3 ,cell_line))  # 3 indicates similarity
+                ground_truth_data.append((dc_row['mu'], 3 ,mutation))  # 3 indicates similarity
                 added_combinations.add(combination)
         # Check against similar drugs
         for drug in similar_drugs:
-            if any(dc.loc[(dc['drug'] == drug) & (dc['cell'] == cell_line), 'rel'] == dc_row['rel']):
-                combination = (dc_row['cell'], cell_line)
+            if any(dc.loc[(dc['drug'] == drug) & (dc['mu'] == mutation), 'rel'] == dc_row['rel']):
+                combination = (dc_row['mu'], mutation)
                 if combination not in added_combinations:
-                    ground_truth_data.append((dc_row['cell'], 3, cell_line))  # 3 indicates similarity
+                    ground_truth_data.append((dc_row['mu'], 3, mutation))  # 3 indicates similarity
                     added_combinations.add(combination)
 
     return ground_truth_data
@@ -83,8 +81,8 @@ def load_and_preprocess_data():
     cc = pd.read_csv('../data/mu_similar0.97.csv')
     dd = pd.read_csv('../data/drug_similar0.78.csv')
 
-    dc.columns = ['cell', 'rel', 'drug']
-    cc.columns = ['cell1', 'rel', 'cell2']
+    dc.columns = ['mu', 'rel', 'drug']
+    cc.columns = ['mu1', 'rel', 'mu2']
     dd.columns = ['drug1', 'rel', 'drug2']
     return dc, cc, dd
 def process_chunk(chunk, dd, cc, dc):
@@ -92,7 +90,7 @@ def process_chunk(chunk, dd, cc, dc):
     for _, row in tqdm(chunk.iterrows(), total=chunk.shape[0]):
         ground_truth = construct_ground_truth(row, dd, cc, dc)
         flat_ground_truth = [item for sublist in ground_truth for item in sublist]
-        ground_truth_rows.append([row['cell'], row['rel'], row['drug']] + flat_ground_truth)
+        ground_truth_rows.append([row['mu'], row['rel'], row['drug']] + flat_ground_truth)
     return ground_truth_rows
 def main():
     dc, cc, dd = load_and_preprocess_data()
